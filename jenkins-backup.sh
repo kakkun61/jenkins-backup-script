47271dfcf356171dfd721f538656ba6f1fce40ee
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-05-15 19:34:08 +0900

    don't archive files & move files directory

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -2,49 +2,36 @@
 
 ##################################################################################
 function usage(){
-  echo "usage: $(basename $0) /path/to/jenkins_home archive.tar.gz"
+  echo "usage: $(basename $0) /path/to/jenkins_home /path/to/destination"
 }
 ##################################################################################
 
 readonly JENKINS_HOME=$1
-readonly DEST_FILE=$2
+readonly DEST_DIR=$2
 readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-readonly TMP_DIR="$CUR_DIR/tmp"
-readonly ARC_NAME="jenkins-backup"
-readonly ARC_DIR="$TMP_DIR/$ARC_NAME"
-readonly TMP_TAR_NAME="archive.tar.gz"
 
-if [ -z "$JENKINS_HOME" -o -z "$DEST_FILE" ] ; then
+if [ $# -ne 2 ] ; then
   usage >&2
   exit 1
 fi
 
-if [[ -f "$TMP_DIR/$TMP_TAR_NAME" ]]; then
-    rm "$TMP_DIR/$TMP_TAR_NAME"
-fi
-rm -rf "$ARC_DIR"
-mkdir -p "$ARC_DIR/"{plugins,jobs,users}
+rm -rf "$DEST_DIR"/*
+mkdir -p "$DEST_DIR/"{plugins,jobs,users}
 
-cp "$JENKINS_HOME/"*.xml "$ARC_DIR"
-cp "$JENKINS_HOME/plugins/"*.[hj]pi "$ARC_DIR/plugins"
+cp "$JENKINS_HOME/"*.xml "$DEST_DIR"
+cp "$JENKINS_HOME/plugins/"*.[hj]pi "$DEST_DIR/plugins"
 hpi_pinned_count=$(find $JENKINS_HOME/plugins/ -name *.hpi.pinned | wc -l)
 jpi_pinned_count=$(find $JENKINS_HOME/plugins/ -name *.jpi.pinned | wc -l)
 if [ $hpi_pinned_count -ne 0 -o $jpi_pinned_count -ne 0 ]; then
-  cp "$JENKINS_HOME/plugins/"*.[hj]pi.pinned "$ARC_DIR/plugins"
+  cp "$JENKINS_HOME/plugins/"*.[hj]pi.pinned "$DEST_DIR/plugins"
 fi
-cp -R "$JENKINS_HOME/users/"* "$ARC_DIR/users"
+cp -R "$JENKINS_HOME/users/"* "$DEST_DIR/users"
 
 cd "$JENKINS_HOME/jobs/"
 ls -1 | while read job_name
 do
-  mkdir -p "$ARC_DIR/jobs/$job_name/"
-  find "$JENKINS_HOME/jobs/$job_name/" -maxdepth 1 -name "*.xml" | xargs -I {} cp {} "$ARC_DIR/jobs/$job_name/"
+  mkdir -p "$DEST_DIR/jobs/$job_name/"
+  find "$JENKINS_HOME/jobs/$job_name/" -maxdepth 1 -name "*.xml" | xargs -I {} cp {} "$DEST_DIR/jobs/$job_name/"
 done
-
-cd "$TMP_DIR"
-tar -czvf "$TMP_DIR/$TMP_TAR_NAME" "$ARC_NAME/"*
-
-cd "$CUR_DIR"
-cp "$TMP_DIR/$TMP_TAR_NAME" "$DEST_FILE"
 
 exit 0
